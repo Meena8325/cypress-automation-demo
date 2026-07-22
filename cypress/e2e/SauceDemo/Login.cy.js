dc6c77c4f1a78381caff9b99f04e688d30d45ad1
Meena Gomani <meena.gomani@gmail.com>
2026-07-22 16:10:29 +0500

    Initial commit: Cypress automation setup with SauceDemo login test

---
+++ cypress/e2e/SauceDemo/Login.cy.js
@@ -0,0 +1,9 @@
+describe('Verify the Login', () => {
+    it('Login with Valid credential and verify the dashboard', () => {
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('#user-name').type('standard_user')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').click()
+        cy.url().should('include', '/inventory.html')
+    })
+})
