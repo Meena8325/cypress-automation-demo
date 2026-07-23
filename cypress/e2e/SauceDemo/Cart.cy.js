77e4f17e6ae882c90929bf68041d9aa77bd533c9
Meena Gomani <meena.gomani@gmail.com>
2026-07-23 10:38:35 +0500

    add to cart, checkout, and remove from cast tests

--- cypress/e2e/SauceDemo/Cart.cy.js
+++ cypress/e2e/SauceDemo/Cart.cy.js
@@ -1,5 +1,5 @@
-describe ("Verify the Add to Cart functionality", () => {
-    it('It should add a product to the cart and verify the cart', () => {
+describe("Verify the Cart functionality shopping cart link clicable", () => {
+    it('It should verify the cart functionality', () => {
         cy.visit('https://www.saucedemo.com/')
         cy.get('#user-name').type('standard_user')
         cy.get('#password').type('secret_sauce')
@@ -7,5 +7,9 @@ describe ("Verify the Add to Cart functionality", () => {
         cy.url().should('include', '/inventory.html')
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
         cy.get('[data-test="shopping-cart-badge"]').should('have.text', '1')
+        cy.get('.shopping_cart_link').click()
+        cy.url().should('include', '/cart.html')
+        cy.get('.cart_item').should('have.length', 1)
     })
 })
+
--- cypress/e2e/SauceDemo/RemoveFromCart.cy.js
+++ cypress/e2e/SauceDemo/RemoveFromCart.cy.js
@@ -1,11 +1,14 @@
-describe ("Verify the Add to Cart functionality", () => {
-    it('It should add a product to the cart and verify the cart', () => {
-        cy.visit('https://www.saucedemo.com/')
+
+describe("Verify the Remove from Cart functionality", () => {
+    it('It should remove a product from the cart and verify the cart', () => {
+        cy.visit('https://www.saucedemo.com/') 
         cy.get('#user-name').type('standard_user')
         cy.get('#password').type('secret_sauce')
         cy.get('#login-button').click()
         cy.url().should('include', '/inventory.html')
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
         cy.get('[data-test="shopping-cart-badge"]').should('have.text', '1')
+        cy.get('[data-test="remove-sauce-labs-backpack"]').click()
+        cy.get('[data-test="shopping-cart-badge"]').should('not.exist')
     })
 })
